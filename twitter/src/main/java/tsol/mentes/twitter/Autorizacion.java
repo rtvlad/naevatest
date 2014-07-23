767338e12cd8c16381f56a3e9b392b29d00c1606
Javier Torresano <jtorresano@naevatec.com>
2014-07-23 13:05:29 +0200

    first example working twitter

---
+++ twitter/src/main/java/tsol/mentes/twitter/Autorizacion.java
@@ -0,0 +1,99 @@
+package tsol.mentes.twitter;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+import twitter4j.conf.ConfigurationBuilder;
+/**
+*
+* @author Javier Torresano
+*
+*/
+public class Autorizacion {
+	
+	
+	 //Constructor de la clase
+	Autorizacion() throws IOException, TwitterException {
+		
+		ConfigurationBuilder configBuilder = new ConfigurationBuilder();
+		configBuilder.setDebugEnabled(true)
+					 .setOAuthConsumerKey("r3cKa2FLDnp5Vw6HhzswclpKF")
+					 .setOAuthConsumerSecret("rnvLn2eWjFO5rGbAopRieVHQtAiCdEvpy0LZ4RvigDEB95sEa2");
+		Twitter OAuthTwitter = new TwitterFactory(configBuilder.build()).getInstance();
+		RequestToken requestToken = null;
+		AccessToken accessToken = null;
+		String url = null;
+		
+		do{
+			try {
+				requestToken = OAuthTwitter.getOAuthRequestToken();
+				System.out.println("Request Token obtenido con éxito.");
+				System.out.println("Request Token: " + requestToken.getToken());
+				System.out.println("Request Token secret: " + requestToken.getTokenSecret());
+				url = requestToken.getAuthorizationURL();
+				System.out.println("URL:");
+				System.out.println(requestToken.getAuthorizationURL());
+			} catch (TwitterException ex) {
+				Logger.getLogger(Twitter.class.getName()).log(Level.SEVERE, null, ex);
+			}
+				BufferedReader lectorTeclado = new BufferedReader(new InputStreamReader(System.in));
+				//Abro el navegador. Firefox, en este caso.
+				Runtime runtime = Runtime.getRuntime();
+			try {
+				runtime.exec("firefox " + url);
+			} catch (Exception e) { }
+			//Nos avisa de que introduciremos el PIN a continuación
+				System.out.print("Introduce el PIN del navegador y pulsa intro.nn PIN: ");
+				//Leemos el PIN
+				String pin = lectorTeclado.readLine();
+				if (pin.length() > 0) {
+					accessToken = OAuthTwitter.getOAuthAccessToken(requestToken, pin);
+				} else {
+					accessToken = OAuthTwitter.getOAuthAccessToken(requestToken);
+				}
+		} while (accessToken==null);
+		
+		
+		System.out.println("nnAccess Tokens obtenidos con éxito.");
+		System.out.println("Access Token: " + accessToken.getToken());
+		System.out.println("Access Token secret: " + accessToken.getTokenSecret());
+		FileOutputStream fileOS = null;
+		File file;
+		String content = accessToken.getToken() + "\n" + accessToken.getTokenSecret();
+		try {
+			file = new File("/home/jtorresano/auth_file.txt");
+			fileOS = new FileOutputStream(file);
+			//Si el archivo no existe, se crea
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+			//Se obtiene el contenido en Bytes
+			byte[] contentInBytes = content.getBytes();
+			fileOS.write(contentInBytes);
+			fileOS.flush();
+			fileOS.close();
+			System.out.println("Escritura realizada con éxito.");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (fileOS != null) {
+					fileOS.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}		
+}
+
---
+++ twitter/src/main/java/tsol/mentes/twitter/TwitterJava.java
@@ -0,0 +1,92 @@
+package tsol.mentes.twitter;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import twitter4j.Paging;
+import twitter4j.ResponseList;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+
+public class TwitterJava {
+	
+	TwitterJava() throws TwitterException {
+		
+		Twitter twitter;
+		ConfigurationBuilder configBuilder = new ConfigurationBuilder();
+		String Token = new String(); //Los almaceno en un string, ya que pueden variar según la cuenta
+		String TokenSecret = new String();
+		File archivo = null;
+		FileReader fileR = null;
+		BufferedReader lecturaTeclado = null;	
+		
+		try {
+				// Apertura del fichero y creacion de BufferedReader
+				archivo = new File("/home/jtorresano/auth_file.txt");
+				fileR = new FileReader(archivo);
+				lecturaTeclado = new BufferedReader(fileR);
+				// Lectura del fichero
+				String linea = new String();
+				int n = 1;
+				while ((linea = lecturaTeclado.readLine()) != null) {
+					if (n == 1) { //La primera línea es el Access Token
+						System.out.println(linea);
+						Token = linea;
+					} else if (n == 2) { //La segunda línea es el Access Token Secret
+						System.out.println(linea);
+						TokenSecret = linea;
+					}
+					n++;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				// En el finally cerramos el fichero, para asegurarnos
+				// que se cierra tanto si todo va bien como si salta
+				// una excepción.
+				try {
+					if (null != fileR) {
+						fileR.close();
+					}
+				} catch (Exception e2) {
+					e2.printStackTrace();
+				}
+			}
+		
+		configBuilder.setDebugEnabled(true)
+			.setOAuthConsumerKey("r3cKa2FLDnp5Vw6HhzswclpKF")
+			.setOAuthConsumerSecret("rnvLn2eWjFO5rGbAopRieVHQtAiCdEvpy0LZ4RvigDEB95sEa2")
+			.setOAuthAccessToken(Token)
+			.setOAuthAccessTokenSecret(TokenSecret);
+		twitter = new TwitterFactory(configBuilder.build()).getInstance();
+		
+		Paging pagina = new Paging();
+		pagina.setCount(40);
+		ResponseList listado = twitter.getHomeTimeline(pagina);
+		for (int i = 0; i < listado.size(); i++) {
+			System.out.println(((Status) listado.get(i)).getText());
+		}
+		//Actualizar tu estado
+		Status tweetEscrito = twitter.updateStatus("[Naeva Tec|Javier Torresano] Twitter+Java - Tweet de prueba desde aplicacion Java");
+	}
+	
+	public static void main(String ar[]) throws TwitterException {
+		try {
+			Autorizacion aut = new Autorizacion();
+		} catch (IOException ex) {
+			Logger.getLogger(TwitterJava.class.getName()).log(Level.SEVERE, null, ex);			
+		} catch (TwitterException ex) {
+			Logger.getLogger(TwitterJava.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		TwitterJava twitter = new TwitterJava();
+	}
+	
+}
